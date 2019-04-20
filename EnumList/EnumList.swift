a22f68051992aee423179ef15db1a324d9b82c8d
A17-Vlad Tkachuk <hustonmen@gmail.com>
2019-04-20 14:55:21 +0300

    fixed enum list and emplemented two ways to ways to get and add element

--- EnumList/EnumList.swift
+++ EnumList/EnumList.swift
@@ -10,85 +10,90 @@ import Foundation
 
 indirect enum EnumList<T>{
 
-    case first
+    case zero
     case node(value: T, next: EnumList?)
     init(){
-        self = .first
+        self = .zero
         
     }
-//    mutating func append(value: T){
-//        self = EnumList.node(value: value, next: self)
-//    }
-
-    mutating func append(value: T!) -> EnumList? {
-        return help(value: value, ref: self)
+    
+    // first way
+    // it add's values like stack but return's like list
+    //                                          !
+    // [zero]<-[next,0]<-[next,1]<-[next,2]<-[next,3]
+    mutating func add(value: T){
+        self = EnumList.node(value: value, next: self)
     }
     
-    mutating func help(value: T!, ref: EnumList) -> EnumList?{
+    // if we wanna get data by index we need to get first node
+    mutating func getAt2(index: Int) -> T?{
+        var i = index
+        return getToFirst(index: &i, ref: self)
+    }
+    
+    private func getToFirst(index: inout Int, ref: EnumList) -> T?{
         switch ref{
-        case .first:
-            self = EnumList.node(value: value, next: nil)
-            return self
+        case .zero:
+            print("got zero") // дійшли до кінця стеку
         case .node(value: let v, next: let next):
-            if next != nil{
-                
-                let up = help(value: value, ref: next!)!
-                self = help(value: v, ref: up)!
+            let returnedValue = getToFirst(index: &index, ref: next!)
+            if returnedValue != nil{
+                return returnedValue
             }
-            else{
-                let new = EnumList.node(value: value, next: nil)
-                return .node(value: v, next: new)
+            else if index == 0{
+                return v
+            }
+            index -= 1
+        }
+        return nil
+    }
+    
+    
+    // second way
+    // works like list
+    mutating func append(value: T!){
+        self = addLast(value: value, ref: self)
+    }
+    
+    private mutating func addLast(value: T, ref: EnumList) -> EnumList{
+        switch ref{
+        case .zero:
+            return .node(value: value, next: .zero)
+        case .node(value: let v, next: let next):
+            let prevnext = addLast(value: value, ref: next!) // в новий нод записується посилання на попередній node(1, ref(0, zero)) ітд
+            return EnumList.node(value: v, next: prevnext)
+        }
+    }
+    
+    mutating func getAt(index: Int) -> T?{
+        if let list = getList(index: index, ref: self){
+            switch list{
+            case .zero:
+                return nil
+            case .node(let value, _):
+                return value
             }
         }
-        return self
+        return nil
     }
     
-//    func merge(_ selv: EnumList,_ new: EnumList){
-//        switch selv{
-//        case .first:
-//            print("")
-//        case .node(value: let v, next: let next, prev: let prev):
-//            if next != nil{
-//                merge(next!, new)
-//            }
-//            else{
-//                return
-//            }
-//            EnumList.node(value: v, next: new, prev: prev)
-//        }
-//    }
-//    func gotEnd(_ value: T, _ current: EnumList){
-//        switch current{
-//        case .first:
-//            print("asd")
-//        case .node(value: let v, next: let next):
-//            if next != nil{
-//                gotEnd(value, next!)
-//            }
-//            else {
-//                let new = EnumList.node(value: value, next: nil)
-//
-//            }
-//
-//        }
-//    }
-    
-//    mutating func getAt(index: Int) -> T?{
-//        if index < 0 {
-//            return nil
-//        }
-//        switch self{
-//        case .first:
-//            return nil
-//            
-//        case .node(let value, let next):
-//            if index == 0 {
-//                return value
-//            }
-//            self = next!
-//        }
-//        return getAt(index: index-1)
-//    }
+    private mutating func getList(index: Int, ref: EnumList) -> EnumList?{
+        var reference = ref
+        if index < 0 {
+            return nil
+        }
+        switch ref{
+        case .zero:
+            return nil
+
+        case .node( _, let next):
+            if index == 0 {
+                return ref
+            }
+            reference = next!
+        }
+        return getList(index: index-1, ref: reference)
+    }
     
     
 }
--- EnumList/EnumList/main.swift
+++ EnumList/EnumList/main.swift
@@ -9,14 +9,23 @@
 import Foundation
 
 var list = EnumList<Int>()
-list = list.append(value: 0)!
+list.append(value: 0)
+list.append(value: 1)
+list.append(value: 2)
+list.append(value: 3)
 
-list = list.append(value: 1)!
+//print(list.getAt(index: 3))
+//print(list.getAt(index: 1))
+//print(list.getAt(index: 0))
+//print(list.getAt(index: 2))
 
-list = list.append(value: 2)!
+var list2 = EnumList<Int>()
+list2.add(value: 0)
+list2.add(value: 1)
+list2.add(value: 2)
+list2.add(value: 3)
 
-list = list.append(value: 3)!
-
-
-print(list.getAt(index: 1))
-print(list.getAt(index: 0))
+print(list2.getAt2(index: 3))
+print(list2.getAt2(index: 1))
+print(list2.getAt2(index: 0))
+print(list2.getAt2(index: 2))
