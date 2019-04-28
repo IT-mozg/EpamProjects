4e60d615bfe558a9cc6835053d38ef2a070c4f8e
A17-Vlad Tkachuk <hustonmen@gmail.com>
2019-04-28 20:44:10 +0300

    calculator done

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -10,11 +10,122 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultPanelLabel: UILabel!
+    var isEntered = false
+    var isDouble = false
+    var firstOperand: Double = 0
+    var secondOperand: Double = 0
+    var operationSing = ""
+    
+    var currentInput:Double{
+        get{
+            return Double(resultPanelLabel.text!)!
+        }
+        set{
+            let value = "\(newValue)"
+            let valueArray = value.components(separatedBy: ".")
+            if valueArray.count == 2 && valueArray[1] == "0"{
+                resultPanelLabel.text = valueArray[0]
+            }
+            else{
+                resultPanelLabel.text = value
+            }
+            isEntered = false
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        resultPanelLabel.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 1/4)
     }
-
-
+    
+    @IBAction func numbers(_ sender: UIButton) {
+        let number = sender.currentTitle!
+        if isEntered{
+            if resultPanelLabel.text!.count < 13{
+                resultPanelLabel.text! += number
+            }
+        }else{
+            if number == "0"{
+                resultPanelLabel.text! = number
+                isEntered = false
+            }
+            else{
+                resultPanelLabel.text! = number
+                isEntered = true
+            }
+        }
+    }
+    
+    @IBAction func twoOperandsSignPresed(_ sender: UIButton) {
+        operationSing = sender.currentTitle!
+        firstOperand = currentInput
+        isEntered = false
+        isDouble = false
+    }
+    
+    func operateWithTwoOperands(operate: (Double, Double)->Double){
+        currentInput = operate(firstOperand, secondOperand)
+        isEntered = false
+    }
+    
+    @IBAction func equalitySingPressed(_ sender: UIButton) {
+        secondOperand = currentInput
+        isDouble = false
+        switch operationSing {
+        case "+":
+            operateWithTwoOperands {$0 + $1}
+        case "-":
+            operateWithTwoOperands {$0 - $1}
+        case "x":
+            operateWithTwoOperands {$0 * $1}
+        case "/":
+            if secondOperand == 0{
+                evokeAlert(title: "Error", message: "Division by zero", style: .actionSheet)
+                break
+            }
+            operateWithTwoOperands {$0 / $1}
+        default:
+            break
+        }
+    }
+    
+    @IBAction func clearButtonPressed(_ sender: UIButton) {
+        firstOperand = 0
+        secondOperand = 0
+        currentInput = 0
+        resultPanelLabel.text = "0"
+        isEntered = false
+        isDouble = false
+        operationSing = ""
+    }
+    
+    @IBAction func plusMinusButtonPressed(_ sender: UIButton) {
+        currentInput *= -1
+    }
+    @IBAction func persentButtonPressed(_ sender: UIButton) {
+        currentInput /= 100
+        isEntered = false
+    }
+    @IBAction func squareRootButtonPressed(_ sender: UIButton) {
+        currentInput = sqrt(currentInput)
+    }
+    
+    @IBAction func pointButtonPressed(_ sender: UIButton) {
+        if isEntered && !isDouble{
+            resultPanelLabel.text! += "."
+            isDouble = true
+        }else if !isEntered && !isDouble{
+            resultPanelLabel.text! = "0.0"
+            isDouble = true
+        }
+    }
+    
+    func evokeAlert(title: String, message: String, style: UIAlertController.Style){
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: style)
+        let action = UIAlertAction(title: "ok", style: .default) { (action) in
+        }
+        alertController.addAction(action)
+        self.present(alertController, animated: true, completion: nil)
+    }
 }
 
