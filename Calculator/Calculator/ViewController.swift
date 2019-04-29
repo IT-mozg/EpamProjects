96b0656e833195d9cb6733b1a9345eaaeefcfc0a
A17-Vlad Tkachuk <hustonmen@gmail.com>
2019-04-29 22:35:44 +0300

    fixed bug with calculating operation value with two operands

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -12,7 +12,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var resultPanelLabel: UILabel!
     var isEntered = false
     var isDouble = false
-    var firstOperand: Double = 0
+    var isPressedOperation = false
+    var firstOperand: Double? = nil
     var secondOperand: Double = 0
     var operationSing = Operation.none
     
@@ -39,6 +40,7 @@ class ViewController: UIViewController {
     
     //MARK: Number pressed
     @IBAction func numbers(_ sender: UIButton) {
+        isPressedOperation = false
         let number = sender.currentTitle!
         if isEntered{
             if resultPanelLabel.text!.count < 13{
@@ -60,26 +62,36 @@ class ViewController: UIViewController {
         firstOperand = currentInput
         isEntered = false
         isDouble = false
+        isPressedOperation = true
     }
     @IBAction func divideButtonPressed() {
+        calculateResultWithTwoOperands()
         operationSing = .div
     }
     @IBAction func muitiplyButtonPressed() {
+        calculateResultWithTwoOperands()
         operationSing = .mult
     }
     @IBAction func subtractButtonPressed() {
+        calculateResultWithTwoOperands()
         operationSing = .sub
     }
     @IBAction func addButtonPressed() {
+        calculateResultWithTwoOperands()
         operationSing = .add
     }
     private func operateWithTwoOperands(operate: (Double, Double)->Double){
-        currentInput = operate(firstOperand, secondOperand)
+        currentInput = operate(firstOperand!, secondOperand)
         isEntered = false
     }
     
-    //MARK: Single operations
-    @IBAction func equalitySingPressed(_ sender: UIButton) {
+    private func calculateResultWithTwoOperands(){
+        guard firstOperand != nil else {
+            return
+        }
+        if isPressedOperation {
+            return
+        }
         secondOperand = currentInput
         isDouble = false
         switch operationSing {
@@ -98,15 +110,23 @@ class ViewController: UIViewController {
         default:
             break
         }
+        firstOperand = nil
+    }
+    
+    //MARK: Single operations
+    @IBAction func equalitySingPressed(_ sender: UIButton) {
+        calculateResultWithTwoOperands()
+        operationSing = .none
     }
     
     @IBAction func clearButtonPressed(_ sender: UIButton) {
-        firstOperand = 0
+        firstOperand = nil
         secondOperand = 0
         currentInput = 0
         resultPanelLabel.text = "0"
         isEntered = false
         isDouble = false
+        isPressedOperation = false
         operationSing = .none
     }
     
@@ -131,7 +151,8 @@ class ViewController: UIViewController {
             resultPanelLabel.text! += "."
             isDouble = true
         }else if !isEntered && !isDouble{
-            resultPanelLabel.text! = "0.0"
+            resultPanelLabel.text! = "0."
+            isEntered = true
             isDouble = true
         }
     }
