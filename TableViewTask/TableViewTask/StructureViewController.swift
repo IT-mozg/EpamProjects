7f2e69d10b6bb2df78fb3ece1ccd3f0e547e0cfc
A17-Vlad Tkachuk <hustonmen@gmail.com>
2019-04-30 15:17:59 +0300

    Created table view task

---
+++ TableViewTask/TableViewTask/StructureViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StructureViewController.swift
+//  TableViewTask
+//
+//  Created by Vlad on 4/30/19.
+//  Copyright © 2019 Vlad Tkachuk. All rights reserved.
+//
+
+import UIKit
+
+class StructureViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
