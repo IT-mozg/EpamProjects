f5c2370dfdfc8c9fe174f6f3b01705a13c8c33f6
A17-Vlad Tkachuk <hustonmen@gmail.com>
2019-05-02 11:28:14 +0300

    created table view

--- TableViewSortsTask1/TableViewSortsTask1/ViewController.swift
+++ TableViewSortsTask1/TableViewSortsTask1/ViewController.swift
@@ -8,12 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+let cellIdentifier = "cell"
 
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    var array: [Int] = []
+    
+    private func getArray(count: Int) -> [Int]{
+        var arr: [Int] = []
+        var item :Int
+        for _ in 0..<count{
+            item = Int(arc4random_uniform(20) + 1)
+            arr.append(item)
+        }
+        return arr
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        array = getArray(count: 10)
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = String(array[indexPath.row])
+        return cell
+    }
+    
+
+    
 
 
 }
