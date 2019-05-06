d2d008d0a8dd9c9036be06c0ec89cdb8c1cf9256
A17-Vlad Tkachuk <hustonmen@gmail.com>
2019-05-06 13:29:26 +0300

    structure changed to optional

--- TableViewTask/TableViewTask/StructureViewController.swift
+++ TableViewTask/TableViewTask/StructureViewController.swift
@@ -16,27 +16,14 @@ class StructureViewController: UIViewController {
     @IBOutlet weak var removeLabel: UILabel!
     @IBOutlet weak var descLabel: UILabel!
     
-    var structure: Structure = Structure(name: "", insertion: "", selection: "", removing: "", description: "")
+    var structure: Structure?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        nameLabel.text = structure.name
-        insertLabel.text = structure.insertion
-        selectLabel.text = structure.selection
-        removeLabel.text = structure.removing
-        descLabel.text = structure.description
-        // Do any additional setup after loading the view.
+        nameLabel.text = structure?.name
+        insertLabel.text = structure?.insertion
+        selectLabel.text = structure?.selection
+        removeLabel.text = structure?.removing
+        descLabel.text = structure?.description
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
