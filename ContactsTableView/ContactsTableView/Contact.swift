6b8967bcb3c5f20593d45fa68c098ed32572064b
A17-Vlad Tkachuk <hustonmen@gmail.com>
2019-05-10 17:33:50 +0300

    implemented encoding and decoding Contact in order to use UserDafaults

--- ContactsTableView/ContactsTableView/Contact.swift
+++ ContactsTableView/ContactsTableView/Contact.swift
@@ -8,7 +8,8 @@
 import UIKit
 import Foundation
 
-struct Contact{
+class Contact: NSObject, NSCoding{
+
     var contactId: String
     var firstName: String
     var lastName: String
@@ -23,5 +24,73 @@ struct Contact{
         self.email = email
         self.phoneNumber = phoneNumber
         self.imagePhoto = imagePhoto
+    }
+    
+    convenience init(id: String, firstName: String, lastName: String, email: String, phoneNumber: String, imagePhoto: UIImage?){
+        self.init(firstName: firstName, lastName: lastName, email: email, phoneNumber: phoneNumber, imagePhoto: imagePhoto)
+        contactId = id
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        let id = aDecoder.decodeObject(forKey: "contactId") as! String
+        let firstName = aDecoder.decodeObject(forKey: "firstName") as! String
+        let lastName = aDecoder.decodeObject(forKey: "lastName") as! String
+        let email = aDecoder.decodeObject(forKey: "email") as! String
+        let phoneNumber = aDecoder.decodeObject(forKey: "phoneNumber") as! String
+        
+        var oldImage: UIImage? = nil
+        let possibleOldImagePath = UserDefaults.standard.object(forKey: "path") as! String?
+        if let oldImagePath = possibleOldImagePath {
+            let pathes = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+            let path = pathes.first! as String
+            let oldFullPath = URL(fileURLWithPath: path).appendingPathComponent(oldImagePath)
+            do{
+                let data = try Data(contentsOf: oldFullPath)
+                oldImage = UIImage(data: data)
+            }catch{
+                oldImage = nil
+            }
+            // here is your saved image:
+        }
+        self.init(id: id, firstName: firstName, lastName: lastName, email: email, phoneNumber: phoneNumber, imagePhoto: oldImage)
+    }
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(contactId, forKey: "contactId")
+        aCoder.encode(firstName, forKey: "firstName")
+        aCoder.encode(lastName, forKey: "lastName")
+        aCoder.encode(email, forKey: "email")
+        aCoder.encode(phoneNumber, forKey: "phoneNumber")
+        encodeImage(imagePhoto!)
+    }
+    
+    func decodeImage() -> UIImage? {
+        let possibleOldImagePath = UserDefaults.standard.object(forKey: "path") as! String?
+        if let oldImagePath = possibleOldImagePath {
+            let oldFullPath = self.documentsPathForFileName(oldImagePath)
+            let oldImageData = NSData(contentsOfFile: oldFullPath.absoluteString)
+            // here is your saved image:
+            let oldImage = UIImage(data: oldImageData! as Data)
+            return oldImage
+        }
+        return nil
+    }
+    
+    func encodeImage(_ image: UIImage){
+        let imgData = UIImage.jpegData(image)
+        let relativePath = "image-\(contactId).jpg"
+        let url = documentsPathForFileName(relativePath)
+        do{
+            try imgData(1.0)?.write(to: url)
+        }catch{}
+        UserDefaults.standard.set(relativePath, forKey: "path")
+        UserDefaults.standard.synchronize()
+    }
+    
+    func documentsPathForFileName(_ name: String) -> URL{
+        let pathes = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+        let path = pathes.first! as String
+        let url = URL(fileURLWithPath: path).appendingPathComponent(name)
+        return url
     }
 }
